a5095450ba80e130d8975a7c2ed538dda0a22390
ianbotella <69222017+ianbotella@users.noreply.github.com>
2024-04-16 17:24:26 +0200

    Update StreamlitDiceRollerApp.py
    "If st.button" no estaba correctamente posicionado... Creo

--- StreamlitDiceRollerApp.py
+++ StreamlitDiceRollerApp.py
@@ -9,13 +9,13 @@ def main():
   num_dice = st.number_input("Enter the number of dice to roll:", min_value=1, value=1)
   sides_per_die = st.number_input("Enter the number of sides per die:", min_value=2, value=4)
   modifier = st.number_input("Enter the modifier to apply to the total roll (can be negative):", value=0)
-
-if st.button("Roll Dice"):
-  roll_total = roll_dice(num_dice, sides_per_die)
-  final_result = roll_total + modifier
-  st.write(f"**Roll Total:** {roll_total}")
-  st.write(f"**Modifier:** {modifier}")
-  st.write(f"**Final Result:** {final_result}")
+  
+  if st.button("Roll Dice"):
+    roll_total = roll_dice(num_dice, sides_per_die)
+    final_result = roll_total + modifier
+    st.write(f"**Roll Total:** {roll_total}")
+    st.write(f"**Modifier:** {modifier}")
+    st.write(f"**Final Result:** {final_result}")
 
 if __name__ == "__main__":
   main()
