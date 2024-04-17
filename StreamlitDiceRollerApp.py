b0ed0d2bcfeaf8d93d23f83bfe53df7d5e888e07
ianbotella <69222017+ianbotella@users.noreply.github.com>
2024-04-17 17:40:13 +0200

    Update StreamlitDiceRollerApp.py

--- StreamlitDiceRollerApp.py
+++ StreamlitDiceRollerApp.py
@@ -42,6 +42,7 @@ def manage_hp():
     with col3:
         if st.button("+"):
             st.session_state.hp += 1
+    st.sidebar.write(f"{st.session_state.hp}")
 
 def display_selected_dice():
     if any(st.session_state.dice_counts.values()):
