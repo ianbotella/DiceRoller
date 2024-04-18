491df566646aaa460ab5cc51151fbff4ebea99c0
ianbotella <69222017+ianbotella@users.noreply.github.com>
2024-04-18 07:35:05 +0200

    Update StreamlitDiceRollerApp.py

--- StreamlitDiceRollerApp.py
+++ StreamlitDiceRollerApp.py
@@ -11,16 +11,15 @@ def setup_session_state():
     if 'dice_counts' not in st.session_state:
         st.session_state.dice_counts = {f"d{num}": 0 for num in [4, 6, 8, 10, 12, 20, 100]}
         st.session_state.results = {}
+        st.session_state.total_hp = 10
+        st.session_state.current_hp = st.session_state.total_hp
         
 def manage_hp():
     st.sidebar.header("HP Management")
-    # Allow the user to set total HP only once or when they want to change it
-    if 'set_total_hp' not in st.session_state or st.sidebar.button("Change Total HP"):
-        st.session_state.set_total_hp = st.sidebar.number_input("Set Total HP", min_value=1, value=10, key="set_total_hp")
-        st.session_state.total_hp = st.session_state.set_total_hp
-        st.session_state.current_hp = st.session_state.total_hp
 
-    st.sidebar.text(f"Total HP: {st.session_state.total_hp}")
+    st.session_state.total_hp = st.sidebar.number_input("Set Total HP", min_value=1, value=st.session_state.total_ho, key="total_hp")
+    st.session_state.current_hp = st.sidebar.number_input("Current HP", min_value=0, max_value=st.session_state.total_hp, value=st.session_state.current_hp, key="current_hp")
+
     col1, col2, col3 = st.sidebar.columns([1, 1, 1])
     
     # Decrement HP
